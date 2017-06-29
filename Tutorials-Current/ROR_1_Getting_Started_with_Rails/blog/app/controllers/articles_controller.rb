30309a878e47202e1a8cb965e1b7592888f86ebb
Jessica Fisher <jessicafisher@gmail.com>
2017-06-29 10:03:52 -0400

    committing tutorial files as of 06.29.2017 at 10.03.00EST

--- Tutorials-Current/ROR_1_Getting_Started_with_Rails/blog/app/controllers/articles_controller.rb
+++ Tutorials-Current/ROR_1_Getting_Started_with_Rails/blog/app/controllers/articles_controller.rb
@@ -8,6 +8,7 @@ class ArticlesController < ApplicationController
 	end
 
 	def new
+		@article = Article.new
 	end
 
 	# Now we are changing the create action to use the new Article model 
@@ -16,8 +17,11 @@ class ArticlesController < ApplicationController
 	def create
 		@article = Article.new(article_params)
 
-		@article.save
-		redirect_to @article
+		if @article.save
+			redirect_to @article
+		else
+			render 'new'
+		end
 	end
 
 	private 
